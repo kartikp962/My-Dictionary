eb5c71d75670057ac05ac6f4daf675a8ef192ee8
Kartik Pal <114161683+kartikp962@users.noreply.github.com>
2023-02-11 13:23:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,51 @@
+const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const searchBtn = document.querySelector("#search-btn");
+const searchInput = document.querySelector("#search-input");
+const wordHeader = document.querySelector("#word-header");
+const wordDefinition = document.querySelector("#word-definition");
+const historyList = document.querySelector("#history-list");
+const clearHistoryBtn = document.querySelector("#clear-history-btn");
+
+let searchHistory = [];
+
+searchBtn.addEventListener("click", async () => {
+  const searchTerm = searchInput.value;
+
+  if (!searchTerm) {
+    return;
+  }
+
+  const response = await fetch(API_URL + searchTerm);
+  const data = await response.json();
+
+  if (data.length === 0) {
+    wordHeader.textContent = "Word not found";
+    wordDefinition.textContent = "";
+    return;
+  }
+
+  const definition = data[0].meanings[0].definitions[0].definition;
+  wordHeader.textContent = searchTerm;
+  wordDefinition.textContent = definition;
+
+  searchHistory.push(searchTerm);
+  renderSearchHistory();
+});
+
+const renderSearchHistory = () => {
+  historyList.innerHTML = "";
+
+  searchHistory.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    historyList.appendChild(listItem);
+  });
+};
+
+clearHistoryBtn.addEventListener("click", () => {
+  searchHistory = [];
+  renderSearchHistory();
+});
+
 
